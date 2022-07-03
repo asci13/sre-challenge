9944fab63aeb824e8509d40303ec8a5842bce48d
Anna Moser <Anna_Moser@outlook.com>
2022-07-03 21:58:49 +0200

    Add push to container registry

--- sre/infrastructure/main.tf
+++ sre/infrastructure/main.tf
@@ -16,19 +16,37 @@ provider "google" {
 
 variable gcp-project-name { default="asci13-sre-challenge" }
 
-resource "google_project_service" "project" {
+module "project-services" {
+  source  = "terraform-google-modules/project-factory/google//modules/project_services"
+  enable_apis = true
+
+  project_id                  = var.gcp-project-name
+
+  activate_apis = [
+    "containerregistry.googleapis.com",
+    "iam.googleapis.com",
+    "cloudresourcemanager.googleapis.com"
+  ]
+}
+
+resource "google_service_account" "service_account" {
+  account_id   = "asci13srechallengeciaccount"
+  display_name = "Challenge CI Account"
   project = var.gcp-project-name
-  service = "containerregistry.googleapis.com"
-
-  timeouts {
-    create = "30m"
-    update = "40m"
-  }
-
-  disable_dependent_services = true
 }
 
 resource "google_container_registry" "registry" {
   project  = var.gcp-project-name
   location = "EU"
+}
+
+resource "google_project_iam_member" "registry_storage_admin" {
+  project = var.gcp-project-name
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_service_account_key" "registry_key" {
+  service_account_id = google_service_account.service_account.name
+  public_key_type    = "TYPE_X509_PEM_FILE"
 }
