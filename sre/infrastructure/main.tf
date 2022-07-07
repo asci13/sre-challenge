abe8d46ddda482eb821f6810b56d394ffdda4a4f
Anna Moser <Anna_Moser@outlook.com>
2022-07-07 22:34:05 +0200

    Update infrastructure prep

--- sre/infrastructure/main.tf
+++ sre/infrastructure/main.tf
@@ -7,41 +7,20 @@ terraform {
   }
 }
 
-provider "google" {
-
-  project = var.gcp-project-name
-  region  = "europe-north1"
-  zone    = "europe-north1-c"
-}
-
-variable gcp-project-name { default="asci13-sre-challenge" }
-
-module "project-services" {
-  source  = "terraform-google-modules/project-factory/google//modules/project_services"
-  enable_apis = true
-
-  project_id                  = var.gcp-project-name
-
-  activate_apis = [
-    "containerregistry.googleapis.com",
-    "iam.googleapis.com",
-    "cloudresourcemanager.googleapis.com"
-  ]
-}
 
 resource "google_service_account" "service_account" {
   account_id   = "asci13srechallengeciaccount"
   display_name = "Challenge CI Account"
-  project = var.gcp-project-name
+  project = var.project_id
 }
 
 resource "google_container_registry" "registry" {
-  project  = var.gcp-project-name
+  project  = var.project_id
   location = "EU"
 }
 
 resource "google_project_iam_member" "registry_storage_admin" {
-  project = var.gcp-project-name
+  project = var.project_id
   role    = "roles/storage.admin"
   member  = "serviceAccount:${google_service_account.service_account.email}"
 }
